9341593f3c46be7b9cc1715679acab4461e8a297
GuenAkira <161044167+GuenAkira@users.noreply.github.com>
2024-05-08 21:47:22 -0500

    Add files via upload

---
+++ InsideTheVietnameseZodiac/VietnameseZodiacController/SnakeController.swift
@@ -0,0 +1,27 @@
+//
+//  SnakeController.swift
+//  InsideTheVietnameseZodiac
+//
+//  Created by Joseph Nguyen on 4/23/24.
+//
+
+import UIKit
+import SwiftUI
+
+class SnakeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let button = UIButton(frame: CGRect(x: 0, y:0, width: 220, height: 50))
+        view.addSubview(button)
+        button.center = view.center
+        button.setTitle("Press for Information", for: .normal)
+        button.backgroundColor = .systemRed
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapButton() {
+        let viewController = UIHostingController(rootView: SnakeView())
+        present(viewController, animated: true)
+    }
+}
---
+++ InsideTheVietnameseZodiac/VietnameseZodiacView/BuffaloView.swift
@@ -0,0 +1,44 @@
+//
+//  BuffaloView.swift
+//  InsideTheVietnameseZodiac
+//
+//  Created by  on 5/3/24.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct BuffaloView: View {
+    var body: some View {
+            List {
+                Text( "Vietnamese Year of the Buffalo")
+                    .font(.largeTitle)
+                ForEach(buffaloTraits) {
+                    traits in
+                    Section(header: Text(traits.title)) {
+                        ForEach(traits.element) {
+                            Element in
+                            Text(Element.name)
+                        }
+                    }
+                }
+                Section(header: Text("You Know You're a Rat Sign If:")) {
+                    Text("Devoted to your family and/or children.")
+                    Text("Don't complain you just deal with what ever situation that comes your way good or bad.")
+                    Text("A great speaker and make superb speeches.")
+                    Text("Determined to work hard to make your dreams come true even if you have walls in your way.")
+                }
+                ForEach(buffaloCompatibility) {
+                    compatibility in
+                    Section(header: Text(compatibility.title)) {
+                        ForEach(compatibility.element) {
+                            Element in
+                            Text(Element.name)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
